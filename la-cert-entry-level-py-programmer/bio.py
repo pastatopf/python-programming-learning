a89fccb57ae30553059229b7b9634a2a04ee16dc
Oscar Santiago <osantiag@gmail.com>
2021-02-22 21:49:37 +0100

    Added jitsi to my deployments

---
+++ la-cert-entry-level-py-programmer/bio.py
@@ -0,0 +1,5 @@
+name = input("What is your name? ")
+color = input("What is your favorite colour? ")
+age = int(input("How old are you today? "))
+
+print(name, "is", age, "years old and like the color", color + ".")
